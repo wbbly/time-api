5f8fb956f996220307c0b2dd91864c2994d60cff
Alexander G <alexgarmatenko@gmail.com>
2019-04-18 15:48:12 +0300

    WOB-62 Hotfix

--- app/src/timer/timer.service.ts
+++ app/src/timer/timer.service.ts
@@ -150,7 +150,7 @@ export class TimerService {
             : '';
 
         const timerStatementArray = [
-            `user: {email: {_in: ["${userEmails.map(userEmail => `"${userEmail}"`).join(',')}"]}}`,
+            `user: {email: {_in: [${userEmails.map(userEmail => `"${userEmail}"`).join(',')}]}}`,
             `start_datetime: {_gte: "${this.timeService.getISOTimeByGivenValue(startDate).slice(0, -1)}"}`,
             `end_datetime: {_lt: "${this.timeService
                 .getISOTimeByGivenValue(this.timeService.getTimestampByGivenValue(endDate) + 24 * 60 * 60 * 1000 - 1)
