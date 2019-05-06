0fed97b017b64157ea04e87e771e9c0228702a1f
Alexander G <alexgarmatenko@gmail.com>
2019-05-06 18:48:57 +0300

    WOB-71 Extend project object

--- app/src/project/interfaces/project.interface.ts
+++ app/src/project/interfaces/project.interface.ts
@@ -1,12 +1,15 @@
 import { ProjectColor } from '../../project-color/interfaces/project-color.interface';
 import { Timer } from '../../timer/interfaces/timer.interface';
+import { Team } from '../../team/interfaces/team.interface';
 
 export interface Project {
     id?: string;
     name?: string;
     isActive?: boolean;
     projectColorId?: string;
+    team_id?: string;
     createdAt?: string;
     projectColor?: ProjectColor;
     timer?: Timer[];
+    team?: Team;
 }
