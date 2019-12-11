5768cf3c9f89eebba62b1dffbde472adcda69545
Alexander G <alexgarmatenko@gmail.com>
2019-12-11 19:36:08 +0200

    VERSION 1.0.7

--- app/src/user/user.service.ts
+++ app/src/user/user.service.ts
@@ -10,7 +10,7 @@ import { SocialService } from '../social/social.service';
 import { AuthService } from '../auth/auth.service';
 import { User } from './interfaces/user.interface';
 
-const APP_VERSION = 'v1.0.5';
+const APP_VERSION = 'v1.0.7';
 const DEFAULT_LANGUAGE = 'en';
 
 @Injectable()
