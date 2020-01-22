3a431410b14b76f19a57486a022edcb5dc18255f
ArtemOdessa <shvets.artem@gmail.com>
2020-01-22 10:35:23 +0200

    create resource adding

--- app/src/resource-planing/resource-planing.controller.ts
+++ app/src/resource-planing/resource-planing.controller.ts
@@ -1,5 +1,7 @@
 import {
-    Controller, Get, Response,
+    Controller,
+    Get,
+    Response,
     HttpStatus,
     Query,
     Post,
@@ -8,18 +10,47 @@ import {
     Param,
     UseGuards,
     Headers,
-    UnauthorizedException, } from '@nestjs/common';
+    UnauthorizedException,
+} from '@nestjs/common';
+
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from '../auth/auth.service';
+import { ResourcePlaningService } from './resource-planing.service';
 
 @Controller('resource-planing')
 export class ResourcePlaningController {
     constructor(
-    ) { }
-    @Get('current')
-    async currentUserResources(@Headers() headers: any, @Response() res: any, @Query() params) {
-        // const userId = await this.authService.getVerifiedUserId(headers.authorization);
-        // if (!userId) {
-        //     throw new UnauthorizedException();
-        // }
-        return res.status(HttpStatus.OK).json({"currentUserResources": "ok"});
+        private readonly authService: AuthService,
+        private readonly ResourcePlaningService: ResourcePlaningService
+    ) {}
+    @Post('add')
+    @UseGuards(AuthGuard())
+    async currentUserResources(@Headers() headers: any, @Response() res: any, @Body() body: any) {
+        const userId = await this.authService.getVerifiedUserId(headers.authorization);
+        if (!userId) {
+            throw new UnauthorizedException();
+        }
+        let resource = null;
+        try {
+            resource = await this.ResourcePlaningService.createPlanResource({
+                userId: body.userId,
+                projectId: body.projectId,
+                teamId: body.teamId,
+                createdById: userId,
+                totalDuration: body.totalDuration,
+                startDate: body.startDate,
+                endDate: body.endDate,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
+        if (resource) {
+            return res.status(HttpStatus.OK).json(resource);
+        }
+
+        return res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json({ message: 'ERROR.PLAN_RESOURCE.CREATE_PLAN_RESOURCE_FAILED' });
     }
 }
