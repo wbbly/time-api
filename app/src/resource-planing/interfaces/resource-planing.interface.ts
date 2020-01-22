a17eafaecba8c8e797f87dee12ba6c042c7f7e98
ArtemOdessa <shvets.artem@gmail.com>
2020-01-22 10:34:37 +0200

    add interface

--- app/src/resource-planing/interfaces/resource-planing.interface.ts
+++ app/src/resource-planing/interfaces/resource-planing.interface.ts
@@ -1,2 +1,13 @@
 export interface ResourcePlaning {
+    id?: string;
+    userId?: string;
+    projectId?: string;
+    teamId?: string;
+    createdById: string;
+    totalDuration?: number;
+    startDate?: string;
+    endDate?: string;
+    createdAt?: string;
+    modifiedAt?: string;
+    userTimeOffId?: string;
 }
