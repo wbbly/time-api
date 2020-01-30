996630c8a7c09a962b1e86823540c95011eef0a1
Alexander G <alexgarmatenko@gmail.com>
2020-01-30 14:33:53 +0200

    UPDATE | Code optimization

--- app/src/resource-planing/resource-planing.service.ts
+++ app/src/resource-planing/resource-planing.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@nestjs/common';
 import { AxiosResponse, AxiosError } from 'axios';
 import moment from 'moment';
 
+import { PlanResource } from './interfaces/resource-planing.interface';
 import { HttpRequestsService } from '../core/http-requests/http-requests.service';
 import { RoleCollaborationService } from '../role-collaboration/role-collaboration.service';
 import { TeamService } from '../team/team.service';
-import { PlanResource } from './interfaces/resource-planing.interface';
 import { TimeService } from '../time/time.service';
 
 @Injectable()
@@ -18,15 +18,16 @@ export class ResourcePlaningService {
         private readonly timeService: TimeService
     ) {}
 
-    async createPlanResource(data: {
+    async createResource(data: {
         userId: string;
         projectId: string;
-        createdById: string;
+        userTimeOffId: string;
         totalDuration: string;
         startDate: string;
         endDate: string;
+        createdById: string;
     }): Promise<AxiosResponse | AxiosError> {
-        const { userId, projectId, createdById, totalDuration, startDate, endDate } = data;
+        const { userId, projectId, userTimeOffId, totalDuration, startDate, endDate, createdById } = data;
 
         const currentTeamData: any = await this.teamService.getCurrentTeam(createdById);
         const isAdmin =
@@ -39,7 +40,8 @@ export class ResourcePlaningService {
                     objects: [
                         {
                             user_id: "${userId}",
-                            project_id: "${projectId}",
+                            project_id: ${projectId ? '"' + projectId + '"' : null},
+                            user_time_off_id: ${userTimeOffId ? '"' + userTimeOffId + '"' : null},
                             created_by_id: "${createdById}",
                             team_id: "${currentTeamData.data.user_team[0].team.id}",
                             total_duration: "${totalDuration}",
@@ -55,19 +57,9 @@ export class ResourcePlaningService {
             }`;
 
             return new Promise((resolve, reject) => {
-                this.httpRequestsService.request(query).subscribe(
-                    async (insertResourceRes: AxiosResponse) => {
-                        const returningRows = insertResourceRes.data.insert_plan_resource.returning;
-                        if (returningRows.length) {
-                            return resolve(returningRows);
-                        } else {
-                            return Promise.reject({
-                                message: 'ERROR.PLAN_RESOURCE.CREATE_PLAN_RESOURCE_REQUEST_TIMEOUT',
-                            });
-                        }
-                    },
-                    (insertResourceError: AxiosError) => reject(insertResourceError)
-                );
+                this.httpRequestsService
+                    .request(query)
+                    .subscribe((res: AxiosResponse) => resolve(res), (error: AxiosError) => reject(error));
             });
         } else {
             return Promise.reject({ message: 'ERROR.USER.NOT.ADMIN' });
@@ -75,40 +67,32 @@ export class ResourcePlaningService {
     }
 
     async getResourceById(id: string): Promise<PlanResource | AxiosError> {
-        const whereStatements = [`id: { _eq: "${id}" }`];
-
         return new Promise((resolve, reject) => {
-            this.getResourceData(whereStatements.join(',')).then(
-                (res: PlanResource) => resolve(res),
-                (error: AxiosError) => reject(error)
-            );
-        });
-    }
-
-    async getResourceData(whereStatement: string): Promise<any | null | AxiosError> {
-        const query = `{
-            plan_resource(where: {${whereStatement}}) {
-                id
-                created_at
-                created_by_id
-                end_date
-                modified_at
-                project_id
-                start_date
-                team_id
-                total_duration
-                user_id
-                user_time_off_id
+            const query = `{
+                plan_resource(
+                    where: {
+                        id: {_eq: "${id}"}
+                    },
+                ) {
+                    id
+                    user_id
+                    project_id
+                    user_time_off_id
+                    team_id
+                    total_duration
+                    start_date
+                    end_date
+                    created_at
+                    modified_at
+                    created_by_id
+                }
             }
-        }
-        `;
+            `;
 
-        let resource: any = null;
-
-        return new Promise((resolve, reject) => {
             this.httpRequestsService.request(query).subscribe(
                 (res: AxiosResponse) => {
                     const resource = res.data.plan_resource.shift();
+
                     return resolve(resource);
                 },
                 (error: AxiosError) => reject(error)
@@ -117,10 +101,9 @@ export class ResourcePlaningService {
     }
 
     async updateResource(
-        resourceId: string,
+        id: string,
         data: {
             projectId: string;
-            teamId: string;
             userId: string;
             totalDuration: number;
             startDate: string;
@@ -129,7 +112,8 @@ export class ResourcePlaningService {
         },
         updatedById: string
     ): Promise<AxiosResponse | AxiosError> {
-        const { projectId, teamId, totalDuration, startDate, endDate, userId, userTimeOffId } = data;
+        const { projectId, totalDuration, startDate, endDate, userId, userTimeOffId } = data;
+
         const currentTeamData: any = await this.teamService.getCurrentTeam(updatedById);
         const isAdmin =
             currentTeamData.data.user_team[0].role_collaboration_id ===
@@ -139,31 +123,31 @@ export class ResourcePlaningService {
             const query = `mutation {
                 update_plan_resource(
                     where: {
-                        id: {_eq: "${resourceId}"}
+                        id: {_eq: "${id}"}
                     },
                     _set: {
+                        user_id: "${userId}",
+                        project_id: ${projectId ? '"' + projectId + '"' : null},
+                        user_time_off_id: ${userTimeOffId ? '"' + userTimeOffId + '"' : null},
+                        team_id: "${currentTeamData.data.user_team[0].team.id}",
+                        total_duration: "${totalDuration}",
+                        start_date: "${startDate}",
                         end_date: "${endDate}",
                         modified_at: "${this.timeService.getISOTime()}",
-                        project_id: ${projectId ? '"' + projectId + '"' : null},
-                        start_date: "${startDate}",
-                        team_id: "${teamId}",
-                        total_duration: "${totalDuration}",
-                        user_id: "${userId}",
-                        user_time_off_id: ${userTimeOffId ? '"' + userTimeOffId + '"' : null},
                     }
                 ) {
                     returning {
                         id
-                        created_at
-                        created_by_id
-                        end_date
-                        modified_at
+                        user_id
                         project_id
-                        start_date
+                        user_time_off_id
                         team_id
                         total_duration
-                        user_id
-                        user_time_off_id
+                        start_date
+                        end_date
+                        created_at
+                        modified_at
+                        created_by_id
                     }
                 }
             }`;
@@ -178,7 +162,7 @@ export class ResourcePlaningService {
         }
     }
 
-    async deleteResourceById(resourceId: string, userId: string): Promise<AxiosResponse | AxiosError> {
+    async deleteResourceById(id: string, userId: string): Promise<AxiosResponse | AxiosError> {
         const currentTeamData: any = await this.teamService.getCurrentTeam(userId);
         const isAdmin =
             currentTeamData.data.user_team[0].role_collaboration_id ===
@@ -186,7 +170,7 @@ export class ResourcePlaningService {
 
         if (isAdmin) {
             const query = `mutation {
-                delete_plan_resource(where: {id: {_eq: "${resourceId}"}}) {
+                delete_plan_resource(where: {id: {_eq: "${id}"}}) {
                     affected_rows
                 }
             }`;
@@ -201,7 +185,90 @@ export class ResourcePlaningService {
         }
     }
 
-    splitPeriodToWeeks(start: string, end: string) {
+    async getResourceList(userIds: any, startDate: string, endDate: string): Promise<any> {
+        const weekPeriods = this.splitDateRangeToWeekPeriods(startDate, endDate);
+        const { weeks, weeksNormalized } = weekPeriods;
+
+        const resourceListUserData = [];
+        weeks.forEach((week: any) => {
+            resourceListUserData.push({
+                startDate: week.start,
+                endDate: week.end,
+                weekNumber: week.weekNumber,
+                plan: [],
+            });
+        });
+
+        const resourceListUsersData = {};
+        userIds.forEach((id: string) => (resourceListUsersData[id] = { data: resourceListUserData }));
+
+        for (const weekNormalized of weeksNormalized) {
+            try {
+                const res = (await this.getUsersResourcesByWeekPeriod(
+                    weekNormalized.start,
+                    weekNormalized.end,
+                    userIds.map((id: string) => `"${id}"`)
+                )) as AxiosResponse;
+
+                const planResourceList = res.data.plan_resource;
+                planResourceList.forEach((userPlanResource: any) => {
+                    const { start_date: startDate, user_id: userId } = userPlanResource;
+                    const weekNumber = moment(startDate, 'YYYY-MM-DDTHH:mm:ss').week();
+
+                    resourceListUsersData[userId].data.forEach((dataObj: any) => {
+                        dataObj.weekNumber === weekNumber ? dataObj.plan.push(userPlanResource) : null;
+                    });
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        return Promise.resolve(resourceListUsersData);
+    }
+
+    private async getUsersResourcesByWeekPeriod(
+        startDate: string,
+        endDate: string,
+        userIds: any
+    ): Promise<AxiosResponse | AxiosError> {
+        // get the current team of the first user from list
+        const currentTeamData: any = await this.teamService.getCurrentTeam(userIds[0]);
+
+        const query = `query{
+            plan_resource(
+                where: {
+                    team_id: {_eq: "${currentTeamData.data.user_team[0].team.id}"} 
+                    user_id: {_in: ${userIds}}, 
+                    _and: [
+                        {start_date: {_gte: "${startDate}", _lte: "${endDate}"}},
+                        {end_date: {_gte: "${startDate}", _lte: "${endDate}"}},
+                    ]
+                },
+                order_by: {start_date: asc}
+            ) {
+                id
+                user_id
+                project_id
+                user_time_off_id
+                team_id
+                total_duration
+                start_date
+                end_date
+                created_at
+                modified_at
+                created_by_id
+            }
+        }`;
+
+        return new Promise(async (resolve, reject) => {
+            this.httpRequestsService
+                .request(query)
+                .subscribe((res: AxiosResponse) => resolve(res), (error: AxiosError) => reject(error));
+        });
+    }
+
+    private splitDateRangeToWeekPeriods(start: string, end: string) {
         const weeks = [];
         const weeksNormalized = [];
 
@@ -228,101 +295,6 @@ export class ResourcePlaningService {
             weeks,
             weeksNormalized,
         };
-    }
-
-    getResourceByWeek(
-        startDate: string,
-        endDate: string,
-        userIds: any,
-        teamId: string
-    ): Promise<AxiosResponse | AxiosError> {
-        const query = `query{
-            plan_resource(
-                where: {
-                    team_id: {_eq: "${teamId}"} 
-                    user_id: {_in: ${userIds}}, 
-                    _or: [
-                        {start_date: {_gte: "${startDate}", _lte: "${endDate}"}},
-                        {end_date: {_gte: "${startDate}", _lte: "${endDate}"}},
-                    ]
-                },
-                order_by: {start_date: asc}
-            ) {
-                id
-                user_id
-                user_time_off_id
-                project_id
-                end_date
-                start_date
-                team_id
-                total_duration
-                created_by_id
-                created_at
-            }
-        }`;
-
-        return new Promise(async (resolve, reject) => {
-            this.httpRequestsService
-                .request(query)
-                .subscribe((res: AxiosResponse) => resolve(res), (error: AxiosError) => reject(error));
-        });
-    }
-
-    async insertUserResources(weeks: any, userIds: any, teamId: string) {
-        let result = {};
-
-        userIds.forEach(id => {
-            result[id] = { data: [] };
-            weeks.weeks.forEach(week => {
-                result[id].data.push({
-                    startDate: week.start,
-                    endDate: week.end,
-                    weekNumber: week.weekNumber,
-                    plan: [],
-                });
-            });
-        });
-
-        const userIdsForQuery = userIds.map(id => `"${id}"`);
-
-        for (let i = 0; i < weeks.weeksNormalized.length; i++) {
-            let resourcesByWeek = null;
-            try {
-                resourcesByWeek = await this.getResourceByWeek(
-                    weeks.weeksNormalized[i].start,
-                    weeks.weeksNormalized[i].end,
-                    userIdsForQuery,
-                    teamId
-                );
-
-                resourcesByWeek.data.plan_resource.forEach(resource => {
-                    const resourceWeekNumber = moment(resource.start_date, 'YYYY-MM-DDTHH:mm:ss').week();
-                    result[resource.user_id].data.forEach(dataObj => {
-                        dataObj.weekNumber === resourceWeekNumber ? dataObj.plan.push(resource) : null;
-                    });
-                });
-            } catch (error) {
-                return Promise.reject({
-                    message: 'ERROR.PLAN_RESOURCE.SHORT_PLAN_RESOURCE_LIST_FAILED',
-                });
-            }
-        }
-
-        return result;
-    }
-
-    async getShortResourceList(userIds: any, startDate: string, endDate: string, userId: string) {
-        const currentTeamData: any = await this.teamService.getCurrentTeam(userId);
-        const weeks = this.splitPeriodToWeeks(startDate, endDate);
-
-        return await this.insertUserResources(weeks, userIds, currentTeamData.data.user_team[0].team.id);
-    }
-
-    async getFullResourceList(userIds: any, startDate: string, endDate: string, userId: string) {
-        const currentTeamData: any = await this.teamService.getCurrentTeam(userId);
-        const weeks = this.splitPeriodToWeeks(startDate, endDate);
-        
-        return await this.insertUserResources(weeks, userIds, currentTeamData.data.user_team[0].team.id);
     }
 
     private getWeekPeriodByDate(date: any) {
