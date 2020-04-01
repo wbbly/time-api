a3afa08f243eece491f03a6204982eb128ef9bee
sepish <sepish@yandex.ru>
2020-04-01 15:53:07 +0300

    #WOB-691 refactor add and edit endpoints for client

--- app/src/client/interfaces/client.interface.ts
+++ app/src/client/interfaces/client.interface.ts
@@ -2,4 +2,12 @@ export interface Client {
     id?: string;
     name?: string;
     project?: [];
+    country?: string;
+    language?: string;
+    state?: string;
+    city?: string;
+    phone?: string;
+    zip?: string;
+    email?: string;
+    avatar?: string;
 }
